ea04283f2c89c7666d96e7c94a8d7bc69b6ea5e0
JianwithQ <147773939@qq.com>
2019-03-07 10:51:03 +0800

    项目初始化

---
+++ lucene_first/src/test/java/Test.java
@@ -0,0 +1,21 @@
+/**
+ * @author Jian.Z
+ * @version v1.0
+ * @date 19/03/06 下午 10:38
+ * @description TODO
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+        //计数器
+        int num=0;
+        int sum;
+        for (sum = 0; sum <=10; sum++) {
+            num=num+sum;
+        }
+        System.out.println(num);
+
+
+    }
+}
